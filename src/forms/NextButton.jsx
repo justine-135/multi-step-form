84a9b35f268085ef2505a4ccc268a4f157a021c2
Justine <ujusine@yahoo.com>
2023-08-27 16:28:00 +0800

    add confirm button on last page

--- src/forms/NextButton.jsx
+++ src/forms/NextButton.jsx
@@ -1,42 +1,62 @@
 import React from "react";
 
-const NextButton = ({ data, setAlert, setSteps, steps }) => {
-  console.log(steps);
+const NextButton = (props) => {
   const handleIncreaseSteps = () => {
-    if (data.name === "") {
-      setAlert((prevAlert) => ({
-        ...prevAlert,
-        name: true,
-      }));
-    }
-    if (data.email === "") {
-      setAlert((prevAlert) => ({
-        ...prevAlert,
-        email: true,
-      }));
-    }
-    if (data.phone === "") {
-      setAlert((prevAlert) => ({
-        ...prevAlert,
-        phone: true,
-      }));
-    }
-    if (data.name !== "" && data.email !== "" && data.phone) {
-      setAlert({
-        name: null,
-        email: null,
-        phone: null,
+    if (props.page == 0) {
+      if (props.data.name === "") {
+        props.setAlert((prevAlert) => ({
+          ...prevAlert,
+          name: true,
+        }));
+      }
+      if (props.data.email === "") {
+        props.setAlert((prevAlert) => ({
+          ...prevAlert,
+          email: true,
+        }));
+      }
+      if (props.data.phone === "") {
+        props.setAlert((prevAlert) => ({
+          ...prevAlert,
+          phone: true,
+        }));
+      }
+      if (
+        props.data.name !== "" &&
+        props.data.email !== "" &&
+        props.data.phone
+      ) {
+        props.setAlert({
+          name: null,
+          email: null,
+          phone: null,
+        });
+        props.setSteps((n) => n + 1);
+      }
+    } else if (props.page == 1) {
+      props.data.plans.map((plan) => {
+        if (plan.selected) {
+          props.setSteps((n) => n + 1);
+        }
       });
-      setSteps((n) => n + 1);
+    } else if (props.page == 2) {
+      props.setSteps((n) => n + 1);
+    } else {
+      console.log("HEy");
     }
   };
   return (
     <div className="navigate-btn-container">
-      <button className="next-btn" onClick={handleIncreaseSteps}>
-        Next Step
-      </button>
-      {steps > 0 && (
-        <button onClick={() => setSteps((n) => n - 1)}>Go back</button>
+      {props.steps !== 3 ? (
+        <button className="next-btn" onClick={handleIncreaseSteps}>
+          Next Step
+        </button>
+      ) : (
+        <button className="next-btn">Confirm</button>
+      )}
+
+      {props.steps > 0 && (
+        <button onClick={() => props.setSteps((n) => n - 1)}>Go back</button>
       )}
     </div>
   );
