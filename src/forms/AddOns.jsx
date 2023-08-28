2c554ecd65836be0cef77cdad09ba36c574e527e
Justine <ujusine@yahoo.com>
2023-08-28 18:47:25 +0800

    update

--- src/forms/AddOns.jsx
+++ src/forms/AddOns.jsx
@@ -20,30 +20,34 @@ const AddOns = ({ setSteps, data, setData, steps }) => {
       <p className="info-text">Add-ons helps enhance your gaming experience.</p>
       <div className="addons-container">
         <ul>
-          {data.addons.map((addon, key) => {
-            return (
-              <li key={key} className={addon.selected && "selected"}>
-                <button
-                  className={addon.selected && "selected"}
-                  onClick={() => handleCheckBox(addon)}
-                >
-                  {addon.selected && (
-                    <img src={require(`../images/${addon.img}`)} alt="" />
-                  )}
-                </button>
-                <div className="addon-info">
-                  <h4>{addon.name}</h4>
-                  <p>{addon.description}</p>
-                </div>
-                <div className="addon-cost">
-                  +$
-                  {data.type === "monthly"
-                    ? `${addon.mo}/mo`
-                    : `${addon.yr}/yr`}
-                </div>
-              </li>
-            );
-          })}
+          {data.addons ? (
+            data.addons.map((addon, key) => {
+              return (
+                <li key={key} className={addon.selected && "selected"}>
+                  <button
+                    className={addon.selected && "selected"}
+                    onClick={() => handleCheckBox(addon)}
+                  >
+                    {addon.selected && (
+                      <img src={require(`../images/${addon.img}`)} alt="" />
+                    )}
+                  </button>
+                  <div className="addon-info">
+                    <h4>{addon.name}</h4>
+                    <p>{addon.description}</p>
+                  </div>
+                  <div className="addon-cost">
+                    +$
+                    {data.type === "monthly"
+                      ? `${addon.mo}/mo`
+                      : `${addon.yr}/yr`}
+                  </div>
+                </li>
+              );
+            })
+          ) : (
+            <li>No Add-ons selected.</li>
+          )}
         </ul>
       </div>
       <NextButton data={data} setSteps={setSteps} steps={steps} page="2" />
